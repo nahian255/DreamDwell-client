4881ce099fa7ab245b8bb72e2fcab3ba9d1c04af
nahian255 <nahimn253@gmail.com>
2023-12-28 09:49:24 +0600

    set-up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <div>
+      <h1>
+        <Home />
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1> d</h1>
+            <h3>i</h3>
+        </div>
+    );
+};
+
+export default Home;
