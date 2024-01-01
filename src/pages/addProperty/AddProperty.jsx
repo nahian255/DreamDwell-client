61313ef7aec642b74f5de5165372c91cce43001a
nahian255 <nahimn253@gmail.com>
2024-01-01 18:55:46 +0600

    mantine install

--- src/pages/addProperty/AddProperty.jsx
+++ src/pages/addProperty/AddProperty.jsx
@@ -1,8 +1,38 @@
 
+import { useState } from 'react';
+import { Stepper, Button, Group } from '@mantine/core';
+
 const AddProperty = () => {
+    const [active, setActive] = useState(1);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+
     return (
-        <div>
+        <div className="p-28 ">
             <h1> Add Property page</h1>
+            <Button variant="filled" color="cyan" size="md" radius="md">Button</Button>
+            <>
+                <Stepper active={active} onStepClick={setActive}>
+                    <Stepper.Step label="First step" description="Create an account">
+                        Step 1 content: Create an account
+                    </Stepper.Step>
+                    <Stepper.Step label="Second step" description="Verify email">
+                        Step 2 content: Verify email
+                    </Stepper.Step>
+                    <Stepper.Step label="Final step" description="Get full access">
+                        Step 3 content: Get full access
+                    </Stepper.Step>
+                    <Stepper.Completed>
+                        Completed, click back button to get to previous step
+                    </Stepper.Completed>
+                </Stepper>
+
+                <Group justify="center" mt="xl">
+                    <Button variant="default" onClick={prevStep}>Back</Button>
+                    <Button className=' text-black border-2 border-gray-300' onClick={nextStep}>Next step</Button>
+                </Group>
+            </>
         </div>
     );
 };
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    // const handelPropertyClick = () => {
+    //     console.log('clicked');
+    // }
     return (
         <div className="bg-slate-200 p-3  flex justify-between px-16">
             <h1 className="text-3xl">DreamDwell</h1>
@@ -8,7 +12,8 @@ const Navbar = () => {
                 <Link to={'/'}>Home</Link>
                 <Link to={'/properites'}>Properites</Link>
                 <Link to={'countact'}>Countact Us</Link>
-                <Link to={'/add-properites'}>Add Property</Link>
+                <Link to={'/add-properites'} >Add Property</Link>
+
             </div>
         </div>
     );
